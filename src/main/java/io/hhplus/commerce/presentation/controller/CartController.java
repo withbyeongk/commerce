5247788acc1270daebd9fd0e5c393d4f07f863f3
withbyeongk <withbyeongk@gmail.com>
2024-10-11 02:40:19 +0900

    feat: 장바구니 mock api 배포
    장바구니 상품 담기
    장바구니 상품 삭제
    장바구니 상품 수량 변경
    장바구니 담긴 상품 목록 조회

--- src/main/java/io/hhplus/commerce/presentation/controller/CartController.java
+++ src/main/java/io/hhplus/commerce/presentation/controller/CartController.java
@@ -1,12 +1,38 @@
 package io.hhplus.commerce.presentation.controller;
 
 import io.hhplus.commerce.application.service.CartService;
+import io.hhplus.commerce.presentation.dto.ProductResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
 public class CartController {
     private final CartService cartService;
 
+    @GetMapping("/cart/products")
+    public void putIn() {
+
+    }
+
+    @DeleteMapping("/cart/products")
+    public void putOff() {
+
+    }
+
+    @GetMapping("/{memberId}/cart/products")
+    public List<ProductResponseDto> getCart(@PathVariable Long memberId) {
+        return List.of(
+                new ProductResponseDto(1L, "Product 1", 100, 50, "Description 1", null, LocalDateTime.now(), LocalDateTime.now()),
+                new ProductResponseDto(2L, "Product 2", 200, 49, "Description 2", null, LocalDateTime.now(), LocalDateTime.now()));
+    }
+
+    @PatchMapping("/{memberId}/cart/products")
+    public void changeQuantity() {
+
+    }
 }
