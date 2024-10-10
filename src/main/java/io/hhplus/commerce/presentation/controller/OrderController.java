9a6d486b9424458fba6d191852faf7efa0b8c205
withbyeongk <withbyeongk@gmail.com>
2024-10-11 02:25:53 +0900

    refact: 결제 api 코드 옮김

--- src/main/java/io/hhplus/commerce/presentation/controller/OrderController.java
+++ src/main/java/io/hhplus/commerce/presentation/controller/OrderController.java
@@ -2,8 +2,6 @@ package io.hhplus.commerce.presentation.controller;
 
 import io.hhplus.commerce.presentation.dto.OrderRequestDTO;
 import io.hhplus.commerce.presentation.dto.OrderResponseDTO;
-import io.hhplus.commerce.presentation.dto.PayRequestDTO;
-import io.hhplus.commerce.presentation.dto.PayResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,12 +18,5 @@ public class OrderController {
     public OrderResponseDTO makeOrder(@RequestBody OrderRequestDTO dto) {
         return new OrderResponseDTO(1L, 2L, 3L, 1000);
     }
-
-    @PostMapping("/pay")
-    public PayResponseDTO pay(@RequestBody PayRequestDTO dto) {
-        return new PayResponseDTO(1L);
-    }
-
-
 
 }
--- src/main/java/io/hhplus/commerce/presentation/controller/PayController.java
+++ src/main/java/io/hhplus/commerce/presentation/controller/PayController.java
@@ -1,10 +1,21 @@
 package io.hhplus.commerce.presentation.controller;
 
+import io.hhplus.commerce.presentation.dto.PayRequestDTO;
+import io.hhplus.commerce.presentation.dto.PayResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/")
 @RequiredArgsConstructor
 public class PayController {
     private final PayController payController;
+
+    @PostMapping("/pay")
+    public PayResponseDTO pay(@RequestBody PayRequestDTO dto) {
+        return new PayResponseDTO(1L);
+    }
 }
