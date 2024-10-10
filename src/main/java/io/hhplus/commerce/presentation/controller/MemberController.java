2c1eeb256052eb40e5e9500c1b522da199a88f67
withbyeongk <withbyeongk@gmail.com>
2024-10-11 01:57:32 +0900

    feat: 회원 잔액 충전 및 조회 mock api 배포

--- src/main/java/io/hhplus/commerce/presentation/controller/MemberController.java
+++ src/main/java/io/hhplus/commerce/presentation/controller/MemberController.java
@@ -1,11 +1,26 @@
 package io.hhplus.commerce.presentation.controller;
 
 import io.hhplus.commerce.application.service.MemberService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+
+    @PostMapping("/points")
+    public void chargePoint(@RequestBody @Param("points") int point, @Param("member_id") Long memberId) {
+
+    }
+
+    @GetMapping("{memberId}")
+    public int getPoint(@PathVariable Long memberId) {
+        return 100;
+    }
+
+
 }
