c37ddcd8cd135a2c885731347bd27d4c4882d2f5
withbyeongk <withbyeongk@gmail.com>
2024-10-15 22:21:35 +0900

    feat: 상품목록 조회 기능 추가, 컨버터 생성하여 pojo to dto 메소드 추가

--- src/main/java/io/hhplus/commerce/application/service/ProductService.java
+++ src/main/java/io/hhplus/commerce/application/service/ProductService.java
@@ -1,17 +1,12 @@
 package io.hhplus.commerce.application.service;
 
 import io.hhplus.commerce.application.util.Converter;
-import io.hhplus.commerce.domain.entity.Product;
 import io.hhplus.commerce.infra.repository.ProductRepository;
 import io.hhplus.commerce.presentation.dto.ProductResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
