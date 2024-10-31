9e03c7dcbc4558e934537ec5330ff80723c45035
withbyeongk <withbyeongk@gmail.com>
2024-10-31 09:57:00 +0900

    refact: 목객체 생성 시 팩토리메서드 사용

---
+++ src/test/java/io/hhplus/commerce/common/DummyFactory.java
@@ -0,0 +1,22 @@
+package io.hhplus.commerce.common;
+
+import io.hhplus.commerce.domain.entity.Member;
+import io.hhplus.commerce.domain.entity.Point;
+import io.hhplus.commerce.domain.entity.Product;
+import io.hhplus.commerce.domain.entity.ProductStock;
+
+public class DummyFactory {
+    public static Member createMember() {
+        return new Member("회원1", 10000);
+    }
+    public static Point createPoint(Long memberId) {
+        return new Point(memberId, 10000);
+    }
+
+    public static Product createProduct() {
+        return new Product("상품1", 1000, 100, "상품1설명");
+    }
+    public static ProductStock createProductStock(Long productId) {
+        return new ProductStock(productId, 100);
+    }
+}
--- src/test/java/io/hhplus/commerce/presentation/controller/MemberControllerConcurrencyTest.java
+++ src/test/java/io/hhplus/commerce/presentation/controller/MemberControllerConcurrencyTest.java
@@ -2,12 +2,10 @@ package io.hhplus.commerce.presentation.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.hhplus.commerce.domain.entity.Member;
-import io.hhplus.commerce.domain.entity.Point;
 import io.hhplus.commerce.infra.repository.MemberRepository;
 import io.hhplus.commerce.infra.repository.PointRepository;
 import io.hhplus.commerce.presentation.controller.member.dto.ChargePointDto;
 import io.hhplus.commerce.presentation.controller.member.dto.PointResponseDto;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +15,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import java.time.LocalDateTime;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import static io.hhplus.commerce.common.DummyFactory.createMember;
+import static io.hhplus.commerce.common.DummyFactory.createPoint;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -41,15 +40,16 @@ public class MemberControllerConcurrencyTest {
     @Autowired
     private PointRepository pointRepository;
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     @Test
     @DisplayName("잔액 충전 동시성 테스트 성공.")
+
     public void chargePointConcurrencyTest() throws Exception {
-        Member member = new Member(null, "회원1", 0, null, null, LocalDateTime.now());
-        Member savedMember = memberRepository.save(member);
-        Point point = new Point(savedMember.getId(), 0);
-        pointRepository.save(point);
+        Member savedMember = memberRepository.save(createMember());
+        pointRepository.save(createPoint(savedMember.getId()));
 
         ExecutorService executorService = Executors.newFixedThreadPool(10);
         CountDownLatch latch = new CountDownLatch(1);
@@ -82,9 +82,9 @@ public class MemberControllerConcurrencyTest {
                 .andReturn();
 
         String content = result.getResponse().getContentAsString();
-        PointResponseDto responseDto = new ObjectMapper().readValue(content, PointResponseDto.class);
+        PointResponseDto responseDto = objectMapper.readValue(content, PointResponseDto.class);
 
-        assertEquals(1000, responseDto.point());
+        assertEquals(11000, responseDto.point());
     }
 
 
